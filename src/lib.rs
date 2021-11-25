0cd046290618ec2bc58c19888d869b22b340b422
zz <517669936@qq.com>
2021-11-25 14:24:55 +0800

    add email header decode support

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,13 @@ pub mod canonicalization;
 pub mod dkim;
 pub mod email;
 pub mod string_tools;
+pub mod header;
 
 mod tests;
 
 pub use self::dkim::Header;
 pub use self::email::Email;
+
+pub(crate) fn find_from(line: &str, ix_start: usize, key: &str) -> Option<usize> {
+    line[ix_start..].find(key).map(|v| ix_start + v)
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,4 +1,4 @@
-// use crate::Email;
+use crate::Email;
 
 #[cfg(test)]
 #[test]
